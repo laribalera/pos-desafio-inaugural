61986c80aa14a41e8660d35c4d2e5e9c4b03b8e4
Larissa Balera <larissa.balera@180s.com.br>
2025-06-06 23:37:07 -0300

    ajustes requisição

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
+function sendForm(event) {
+    event.preventDefault();
 
-function sendForm(){
-    
-const url = 'https://fsdt-contact.onrender.com/contact'
-const form = {
-    "names": [
-        document.getElementById('nome1').value,
-        document.getElementById('nome2').value,
-        document.getElementById('nome3').value,
-        document.getElementById('nome4').value,
-        document.getElementById('nome5').value
-    ],
-    "message": document.getElementById('mensagem').value
-}
+    const url = 'https://fsdt-contact.onrender.com/contact';
+    const form = {
+        "names": [
+            document.getElementById('nome1').value,
+            document.getElementById('nome2').value,
+            document.getElementById('nome3').value,
+            document.getElementById('nome4').value,
+            document.getElementById('nome5').value
+        ],
+        "message": document.getElementById('mensagem').value
+    };
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
-})
-    .then(response => response.json())
-    .then(json => {
-        alert("Mensagem enviada com sucesso!");
-        console.log("resposta:", json);
-        document.getElementById('nome1').value.reset();
-        document.getElementById('nome2').reset();
-        document.getElementById('nome3').reset();
-        document.getElementById('nome4').reset();
-        document.getElementById('nome5').reset();
-        document.getElementById('mensagem').reset();
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(form)
     })
-    .catch(error => {
-        console.error("Erro ao fazer a requisição:", error);
-    })
+        .then(response => response.json())
+        .then(json => {
+            alert("mensagem enviada com sucesso!");
+            console.log("resposta:", json);
+
+            document.getElementById('nome1').value = "";
+            document.getElementById('nome2').value = "";
+            document.getElementById('nome3').value = "";
+            document.getElementById('nome4').value = "";
+            document.getElementById('nome5').value = "";
+            document.getElementById('mensagem').value = "";
+        })
+        .catch(error => {
+            console.error("erro na requisição:", error);
+        });
 }
